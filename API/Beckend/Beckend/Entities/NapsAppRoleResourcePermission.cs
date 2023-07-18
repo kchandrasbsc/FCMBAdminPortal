1476d1b3abb6aac8c6017ad0b5df5627782bf197
Yogesh Pant <ypant@sbsc.com>
2023-07-18 09:55:47 +0530

    Backend user role update context from Test update

---
+++ API/Beckend/Beckend/Entities/NapsAppRoleResourcePermission.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beckend.Entities;
+
+public partial class NapsAppRoleResourcePermission
+{
+    public decimal RolesId { get; set; }
+
+    public decimal PermissionsId { get; set; }
+}
---
+++ API/Beckend/Beckend/Entities/NapsMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beckend.Entities;
+
+public partial class NapsMenuItem
+{
+    public long Id { get; set; }
+
+    public string? Access { get; set; }
+
+    public bool Active { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Ordering { get; set; }
+
+    public long? MenuId { get; set; }
+}
